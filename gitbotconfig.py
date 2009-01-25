15e0b26b979391f8be9516ec401e438eed982f94
tav <tav@espians.com>
2009-01-25 10:35:22 +0000

    An IRC notifier for GitHub commits.

---
+++ gitbotconfig.py
@@ -0,0 +1,8 @@
+REPOSITORIES  = {
+
+    ('tav', 'plexnet'): ['#esp'],
+    ('tav', 'blog'): ['#esp'],
+    ('tav', 'scripts', 'gh-pages'): ['#esp'],
+    ('tav', 'static', 'gh-pages'): ['#esp'],
+    
+    }
